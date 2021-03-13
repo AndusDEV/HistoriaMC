c34446752e3415a9f57c017512c062972ec8f0e1
resres2007@gmail.com <30293380+AndusDEV@users.noreply.github.com>
2021-03-13 18:04:03 +0100

    Jar file, and some changes to versions text

--- src/pl/andus/McHistory/Main.java
+++ src/pl/andus/McHistory/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String[] MCver = {"1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8 - rewolucyjne", "1.9", "1.10", "1.11", "1.12", "1.13 - rewolucyjne", "1.14 - rewolucyjne", "1.15", "1.16 - rewolucyjne", "1.17 - nadchodzące, rewolucyjne"};
+        String[] MCver = {"1.0 - najstarsza", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8 - rewolucyjna", "1.9", "1.10", "1.11", "1.12", "1.13 - rewolucyjna", "1.14 - rewolucyjna", "1.15", "1.16 - rewolucyjna", "1.17 - nadchodząca, rewolucyjna, najnowsza"};
         for (int i = 0; i < MCver.length; i++) {
             System.out.println(MCver[i]);
         }
